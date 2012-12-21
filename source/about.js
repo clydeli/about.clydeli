2e189029a07535019f8b28698c1f93fe6a1a86a1
clydeli <clyde1008li@gmail.com>
2012-12-20 22:23:42 -0800

    Refactoring in Progress

---
+++ source/about.js
@@ -0,0 +1,289 @@
+﻿// Background image information
+var BG_IMG_LIST = [];
+BG_IMG_LIST.push(new bg_img('https://lh3.googleusercontent.com/-5y1dQeU3yYw/TjJ7aDf8aXI/AAAAAAAAKBE/wxGVszezlAc/s1440/P1010606.JPG', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5634698332240377633/5634701771340015986" >Shanghai, China  Aug. 2010 </a>', 3/4, 0.55));
+BG_IMG_LIST.push(new bg_img('https://lh5.googleusercontent.com/-zFqyF61Qv6s/TSk_1i5e9_I/AAAAAAAAIao/QFGK2_iM4Yw/s1440/20100731836.jpg', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5560041465274782513/5560045404099114994" >Tokyo, Japan  Jul. 2010 </a>', 3/4, 0.6));
+BG_IMG_LIST.push(new bg_img('https://lh3.googleusercontent.com/-Tx8HuKsKgRE/SOJE5vsy8II/AAAAAAAACeE/bhkT8AGNAsc/s1440/20080824868.jpg', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5251833612796485585/5251835874315006082" >Alishan, Taiwan  Aug. 2008 </a>', 3/4, 0.65));
+BG_IMG_LIST.push(new bg_img('https://lh4.googleusercontent.com/-g2m2CwCxBb0/TCyjVNdnKEI/AAAAAAAAGbs/mM3s5dm_V3o/s1440/20100630527.jpg', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5488941284228439281/5488941630644234306?authkey=CKiw89DP_vWYNg" >Google Taiwan  Jun. 2010 </a>', 0.75, 0.6));
+BG_IMG_LIST.push(new bg_img('https://lh4.googleusercontent.com/-qAPXCuFQ4X4/TU1oHPLax4I/AAAAAAAAJMQ/eNsnuaawGOU/s1440/20100622507.jpg', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5570209861323780065/5570222787669968770" >HSNU, Taiwan  Jun. 2010 </a>', 3/4, 0.3));
+BG_IMG_LIST.push(new bg_img('https://lh3.googleusercontent.com/-WKls_I1sTvI/SODl7NqSa7I/AAAAAAAACVU/lWIFgScJ0qU/s1440/20080813769.jpg', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5251446656504758577/5251449970956069810" >Bitoujiao, Taiwan  Aug. 2008 </a>', 3/4, 0.5));
+BG_IMG_LIST.push(new bg_img('https://lh4.googleusercontent.com/-MWw7U58wmy4/UCnrIgVj6JI/AAAAAAAAPu8/klDwpEALN0A/s1440/P1130116.JPG', '<a href="https://plus.google.com/photos/106431348054423810177/albums/5776405569811745793/5776406528431220882" >Taipei, Taiwan  Sep. 2012 </a>', 9/16, 0.5));
+
+// Portfolio tags information
+var TAGS_INFO_OBJ = new tags_info();
+TAGS_INFO_OBJ.insert_type('lang', 'LightBlue', ['C','C++','C#','Java','HTML','CSS','JS','verilog'])
+TAGS_INFO_OBJ.insert_type('field', 'LightCoral', ['web_dev','architecture','algorithm','UI','app','game'])
+TAGS_INFO_OBJ.insert_type('type', 'Khaki', ['research','project','book','patent'])
+TAGS_INFO_OBJ.insert_type('genre', 'LightGreen', ['indie','NTU'])
+
+// Randomize a background image
+var now = new Date();
+var BG_NUM = Math.floor(Math.random(now.getSeconds())*BG_IMG_LIST.length);
+
+// Portofolio collapse/expand functions
+function collapse_portfolio(is_all, p_id){
+  var selector = (is_all)? '.portfolio':'#p_id_'+p_id;
+  $(selector).removeClass('p_expanded');
+}
+function expand_portfolio(p_id){
+  collapse_portfolio(true);
+  $('#p_id_'+p_id).addClass('p_expanded');
+  $('#portfolio_tags_cloud').after($('#p_id_'+p_id));
+  // fadeIn effect
+  $('.portfolio').hide();
+  $('#p_id_'+p_id).fadeIn("slow");
+}
+
+// Portofolio filter/refresh functions
+function filter_portfolio_tags(tag_name){
+  var is_checked = TAGS_INFO_OBJ.get_name_checked(tag_name);
+  $('#portfolio_tags_cloud .portfolio_tags li').each( function(){
+    if($(this).text() == tag_name){ (is_checked)? $(this).addClass('checked'):$(this).removeClass('checked'); }
+  });
+
+  var matched_pid_list = TAGS_INFO_OBJ.get_pid_list(tag_name);
+  for(var i=matched_pid_list.length; i>=0; --i){
+    var should_hide = true;
+    $('#p_id_'+matched_pid_list[i]+' .portfolio_tags li').each( function(){
+      if($(this).text() == tag_name){ 
+        if(is_checked){
+          $(this).addClass('checked')
+          $('#portfolio_tags_cloud').after($('#p_id_'+matched_pid_list[i]));
+        } else { $(this).removeClass('checked'); }
+      }
+      if($(this).hasClass('checked')){ should_hide = false; }
+    });
+    (should_hide)? $('#p_id_'+matched_pid_list[i]).fadeOut("slow"):$('#p_id_'+matched_pid_list[i]).fadeIn("slow");
+  }
+}
+function refresh_portfolio_display(){
+  $('.portfolio').each( function(){
+    var should_hide = true;
+    $(this).find('.portfolio_tags li').each( function(){
+      if($(this).hasClass('checked')){ should_hide = false; }
+    });
+    (should_hide)? $(this).hide():$(this).show();
+  });
+}
+
+
+// Display functions
+function SHOW_AREA(hash_vars) {
+  // navbar manipulation
+  $('#menu_header ul li').removeClass("active");
+  $('#'+hash_vars['p']+'_btn').addClass("active");
+
+  // clearout areas
+  $('.area').hide();
+
+  switch(hash_vars['p']){
+    case 'vitae':
+      $('#vitae').fadeIn('slow');
+      break;
+    case 'portfolio':
+      collapse_portfolio(true);
+      $('#portfolio_tags_cloud, .portfolio').fadeIn('slow');
+      refresh_portfolio_display();
+      if(hash_vars['p_id'] != undefined){
+        // Ensure that content img is loaded before expansion
+        //$('#p_id_'+hash_vars['p_id']+' .portfolio_content_img')[0].onload  = function() { expand_portfolio(hash_vars['p_id']); }
+        expand_portfolio(hash_vars['p_id']);
+      }
+      break;
+	  case 'misc':
+      $('#misc').fadeIn('slow');
+      break;
+    default:
+      $('#home_btn').addClass("active")
+      $('#about_profile, #about_text').fadeIn('slow');
+      break;
+  }
+}
+
+function BG_CROPPING(){
+  // Position the horizontal frame 
+  $('#hor_frame').css('height', $('body').width()*27/64+'px').css('top', ($('body').height()-$('#hor_frame').height())/2 );
+  $('#copyright_footer').css('top', ($('body').height()-$('#hor_frame').height())/2+ 'px')
+  //$('#hor_frame').css('width', $('body').width() + 'px'); // 1px frame issue
+  // Position the background frame
+  $('#bg_frame').css('width', $('body').width()*0.95+'px');
+  $('#bg_frame').css('height', $('#hor_frame').height()+'px'); // CHROME DEV HACK
+  // Calculate and position(center-aligning) background img
+  var neg_offset = -($('#bg_frame').width()*BG_IMG_LIST[BG_NUM].get_ratio()*BG_IMG_LIST[BG_NUM].get_center() - $('#bg_frame').height()/2);
+  var pos_offset = $('#bg_frame').width()*BG_IMG_LIST[BG_NUM].get_ratio()-$('#bg_frame').height();
+  if(pos_offset < -neg_offset){ neg_offset = -pos_offset; }
+  var bg_pos = (neg_offset >= 0)? '0 0':'0 '+neg_offset+'px';
+  $('#bg_frame, #hor_frame').css('background-position', bg_pos);
+  
+  // Header and Footer
+  //$('#main_frame').css('width', $('#main_frame').width() + 'px');
+  $('#main_frame').css('height', $('#bg_frame').height()-40 + 'px');
+  $('#menu_header, #page_footer').css('width', $('#hor_frame').width() +'px');
+  $('#menu_header, #page_footer').css('margin-left', ($('#bg_frame').width()-$('#hor_frame').width())/2+'px');
+
+  // Make right float compatible with 104% scroll hide HACK
+  $('#misc').css('margin-right', $('#main_frame').width()-$('#bg_frame').width()+16);
+};
+
+$(document).ready(function(){
+
+  // Generate portfolio HTML 
+  for(var i=portfolio_entries.length-1; i>=0; --i){
+    var p_id = portfolio_entries[i]['md5_pid'];
+    var p_entry = 
+      '<section class="area portfolio" id="p_id_'+p_id+'"> <img class="portfolio_thumb_img" alt="" src="imgs/portf_'+p_id+'_thumb.jpg">'+
+        '<a href="#!p=portfolio&p_id='+p_id+'"><div class="portfolio_title"> <h2>.'+portfolio_entries[i]['title']+'</h2></div></a>'+
+          '<div class="portfolio_content">'+
+            '<div class="portfolio_fn">.collapse( )</div>'+
+            '<article> <header>.'+portfolio_entries[i]['header']+'</header>'+
+              '<div class="portfolio_content_img" style="background-image: url(imgs/portf_'+p_id+'.jpg);"><div class="portfolio_exp"><h2>';
+              //'<div class="portfolio_exp"><h2>';
+    
+    // Iterate through meta information
+    if(portfolio_entries[i].hasOwnProperty('meta_expansion') ){      
+      for(var j=0; j<portfolio_entries[i]['meta_expansion'].length; ++j ){
+        p_entry +=
+          '<b>'+portfolio_entries[i]['meta_expansion'][j]['label']+': </b>'+
+          portfolio_entries[i]['meta_expansion'][j]['content'] + '<br>';
+      }
+    }
+
+    // Iterate through links
+    if(portfolio_entries[i].hasOwnProperty('links') ){
+      p_entry += '<b>Links: </b>';
+      for(var j=0; j<portfolio_entries[i]['links'].length; ++j ){
+        p_entry += '[<a href="'+portfolio_entries[i]['links'][j]['href']+'">'+portfolio_entries[i]['links'][j]['label']+'</a>] ';
+      }
+      p_entry += '<br>';
+    }
+
+    // Iterate through images
+    /*if(portfolio_entries[i].hasOwnProperty('images') ){
+      p_entry += '<b>Additional Images: </b><br>';
+      for(var j=0; j<portfolio_entries[i]['images'].length; ++j ){
+        p_entry += '<img class="content_thumb_img" alt="" src="'+portfolio_entries[i]['images'][j]+'">';
+      }
+      p_entry += '<br>';
+    }*/
+
+
+    // Introduction and close previous tags and open 'tags' tag
+    p_entry += '</h2></div></div><b>Introduction:</b><br>'+portfolio_entries[i]['introduction']+'</article> </div> <ol class="portfolio_tags">';
+                        
+    // Iterate through tags      
+    for(var j=portfolio_entries[i]['tags'].length-1; j>=0; --j ){
+      p_entry += '<li>'+portfolio_entries[i]['tags'][j]+'</li> ';
+      TAGS_INFO_OBJ.insert_pid( portfolio_entries[i]['tags'][j], p_id);
+    }
+    $("#portfolio_tags_cloud").after( p_entry+'</ol></section>' );
+  }
+
+  // Global behavior initialzation 
+  $('#page_footer a, #vitae a, .social_list a, .portfolio .portfolio_exp a, #about_text a').attr('rel', 'external'); // set rel=external 
+  $("a[rel='external']").attr('target', '_blank');
+
+
+  // Portfolio tags settings
+  var tags_list = TAGS_INFO_OBJ.get_name_type_hash();
+  var first_element = true; // tags reverse hack
+  for(var key in tags_list){
+    if(tags_list.hasOwnProperty(key)) {
+      if(first_element){
+        $('#portfolio_tags_cloud .portfolio_tags').prepend('<li>'+key+'</li>');
+        first_element = false;
+      }
+      else{ $('#portfolio_tags_cloud .portfolio_tags li:last').after(' <li>'+key+'</li> '); }
+    }
+  }
+
+  //$('.portfolio_tags li').addClass('checked'); // If use select all as default
+  $('.portfolio_tags li').each( function(){
+    $(this).css('background-color', TAGS_INFO_OBJ.get_color( TAGS_INFO_OBJ.get_type( $(this).text())));
+  });
+
+  // Background image settings
+  $('#bg_frame, #hor_frame').css('background-image', 'url('+BG_IMG_LIST[BG_NUM].get_src()+')');
+  $('#page_footer .bg_info').append(BG_IMG_LIST[BG_NUM].get_info());
+  BG_CROPPING();
+  
+  $(window).resize(function() { BG_CROPPING(); });
+  
+  // Decode hash tag information
+  var hash_vars = [];
+  var hashes = unescape(self.document.location.hash.slice(2)).split('&');
+  for(var i=0; i < hashes.length; ++i){
+    var hash_var = hashes[i].split('=');    
+    hash_vars[hash_var[0]] = hash_var[1];
+  }
+  SHOW_AREA(hash_vars);
+  
+  // Listen on popstate event
+  /*window.onpopstate = function(){
+    var hash_vars = [];
+    var hashes = unescape(self.document.location.hash.slice(2)).split('&');
+    for(var i=0; i < hashes.length; ++i){
+      var hash_var = hashes[i].split('=');    
+      hash_vars[hash_var[0]] = hash_var[1];
+    }
+    SHOW_AREA(hash_vars);
+  }*/
+
+  // Menu button triggers
+  $('#menu_header nav ul li').click(function() {
+    SHOW_AREA( {"p": $(this).attr('id').slice(0, -4)} );
+  });
+
+  // Portfolio triggers
+  $("#main_frame").on("click", ".portfolio .portfolio_title", function(){
+    expand_portfolio( $(this).parents('.portfolio').attr('id').slice(5) );
+  });
+  $("#main_frame").on("click", ".portfolio .portfolio_fn", function(){
+    collapse_portfolio(false, $(this).parents('.portfolio').attr('id').slice(5) ); 
+    refresh_portfolio_display();
+  });
+  $('.portfolio_tags li').click(function() {
+    collapse_portfolio(true);
+    refresh_portfolio_display();
+    TAGS_INFO_OBJ.set_name_checked('toggle', $(this).text());
+    filter_portfolio_tags($(this).text());
+  });
+  $('#portfolio_tags_cloud .portfolio_fn').click(function(){
+    collapse_portfolio(true);
+    switch($(this).text()){
+      case '.select("all")':
+        TAGS_INFO_OBJ.set_name_checked("all");
+        $('.portfolio_tags li').addClass('checked');
+        $('.portfolio').show();
+        break;
+      case '.select("none")':
+        TAGS_INFO_OBJ.set_name_checked("none");
+        $('.portfolio_tags li').removeClass('checked');
+        $('.portfolio').hide();
+        break;
+    }
+  });
+
+  // Pop image triggers
+  $(".portfolio .content_thumb_img").hover( function() {
+    $('#pop_img').css('top', $(this).offset().top).css('left', $(this).offset().left+$(this).width()+2)
+    $("#pop_img img").attr("src", $(this).attr("src"));
+    $("#pop_img").stop(true, true).fadeIn("normal");
+  }, function() { $("#pop_img").fadeOut("normal"); }); 
+
+  // misc function triggers
+  $('#misc .misc_fn').click(function(){
+    switch($(this).text()){
+      case '.background.random( )':
+        BG_NUM = Math.floor(Math.random(now.getSeconds())*BG_IMG_LIST.length);
+        break;
+      case '.background.next( )':
+        BG_NUM = (BG_NUM+1) % BG_IMG_LIST.length;
+        break;
+    }
+    if ($(this).text().slice(1,11) == 'background'){
+      $('#bg_frame, #hor_frame').css('background-image', 'url('+BG_IMG_LIST[BG_NUM].get_src()+')');
+      $('#page_footer .bg_info').html(BG_IMG_LIST[BG_NUM].get_info());
+      BG_CROPPING();
+    }
+  });
+
+});
---
+++ source/portfolio_objs.js
@@ -0,0 +1,105 @@
+var portfolio_entries = [
+{
+	md5_pid: "",
+	title: "Method and System for Managing SQL-NoSQL Hybrid Database",
+	header: "Method and System for Managing SQL-NoSQL Hybrid Database",
+	year: "Feb. 2012",
+	meta_expansion:
+	  [ { label: "Co-Authors", content: "Chen-Mou Cheng, An-Tai Li, Song-Ming Wu" } ],
+	introduction: '',
+	tags: ["architecture", "patent", "research", "NTU"]
+},
+{
+	md5_pid: "d64ed8a4d9795df5ec63a8a344672f7b",
+	title: "tagSearch",
+	header: "tagSearch - Search by Tags",
+	year: "Feb. 2012",
+	links : [ { label: "website", href: "http://apps.clydeli.com/tagSearch" } ],
+	introduction: 'Most search engines provide us with advanced search abilities by using specified operators to make accurate queries. However, not every typical user makes use of it.<br><br> Integrating the idea to the GUI level by transforming keywords to tags with colors make the concept more clear and adaptable. The new interface also provides a more intuitive experience of manipulating and modifying keywords, which is always good especially for users on devices with touch screens.',
+	tags: ["HTML", "CSS", "JS", "UI", "web_dev", "indie"]
+},
+{
+	md5_pid: "797ab3a6d11c6b032067fc6405ba2d5b",
+	title: "Minimal Register Usage Instruction Scheduling",
+	header: "Minimal Register Usage Ins. Scheduling",
+	meta_expansion:
+	  [ { label: "Research Members", content: "Chiang-Nan Lin, An-Tai Li" },
+	    { label: "Advisor", content: "Professor Chen-Mou Cheng, Ph.D." } ],
+	introduction: 'Combined backtracking search and constraint programming to find optimal instruction schedule that used minimal number of registers. By providing effective constraints, we could find optimal solutions for basic blocks with up to about one-hundred and fifty instructions.<br><br>',
+   	tags: ["C++", "algorithm", "research", "NTU"]
+},
+{
+	md5_pid: "c416e2311cc1b57378f9cb33fccd6fd7",
+	title: "Programming with Visual C# 2010 in 16 Lessons",
+	header: "Programming with Visual C# 2010 in 16 Lessons",
+	meta_expansion:
+	  [ { label: "Co-Authors", content: "Chi-Lung Lee, An-Tai Li" },
+	  	{ label: "Chinese Title", content: "Visual C# 2010 程式設計16堂課" },
+	  	{ label: "Publishing", content: "by GOTOP Information Inc.(碁峰資訊) in April, 2011" },
+	  	{ label: "ISBN", content: "9789862761816" } ],
+	links : [ {label: "博客來", href: "http://www.books.com.tw/exep/prod/booksfile.php?item=0010504523" } ],
+	introduction: 'A book to help you learn the lastest version of Microsoft Visual C# development package in 16 lessons. With rich console and windows examples, the book provides solid fundamentals of c# programming and complete project building experience. The book is written in Traditional Chinese.',
+   	tags: ["C#", "book", "indie"]
+},
+{
+	md5_pid: "",
+	title: "unofficial Google Images instant",
+	header: "unofficial Google Images instant",
+	links : [ { label: "website", href: "http://apps.clydeli.com/gi_instant" } ],
+	introduction: 'A quick web application which is apparently inspired by Google Instant. Search results display on-the-fly when user input query strings. Google web search API is used.<br><br> update: Google has officially launched their beta test of Google Images Instant and Google Web Search API is also deprecated. So this site will not be updated in the future.',
+   	tags: ["HTML", "CSS", "JS", "web_dev", "indie"]
+},
+{
+	md5_pid: "59bdf583926b496de784a510f0fb4391",
+	title: "ARble Maze",
+	header: "ARble Maze",
+	meta_expansion:
+	  [ { label: "Team Members", content: "Ming-Lun Chou, An-Tai Li, Zhan-Rong Kuo" },
+	  	{ label: "Role", content: "Toolkits Linking, System Integration and Stage Design" },
+	  	{ label: "Course", content: "Network and Multimedia Lab" },
+	  	{ label: "Instructor", content: "Professor Hung-Yu Wei, Ph.D." } ],
+	images: 
+	  [ "imgs/portf_WEFFKN_1.jpg", "imgs/portf_WEFFKN_2.jpg", "imgs/portf_WEFFKN_3.jpg" ],
+	introduction: 'Developed augmented reality marble maze game written in Java. User controls the maze through webcam. Program is written in Java with NyARToolkit(Augmented Reality), JBox2D(Physics), JMonkey(3D Graphic) libraries.',
+   	tags: ["Java", "game", "UI", "project", "NTU"]
+},
+{
+	md5_pid: "",
+	title: "CPU with Crypto Instructions",
+	header: "CPU with Crypto Instructions",
+	meta_expansion:
+	  [ { label: "Research Members", content: "Jakub Szefer, Ming-Shing Chen, An-Tai Li" },
+	  	{ label: "Advisor", content: "Professor Chen-Mou Cheng, Ph.D." } ],
+	introduction: 'Designed and implemented a CPU in hardware description level with built-in special designed instructions for Crypto, targeting on AES/MPKC.',
+   	tags: ["verilog", "architecture", "research", "NTU"]
+},
+{
+	md5_pid: "",
+	title: "Taipei Metro Pricing Query System",
+	header: "Taipei Metro Pricing Query System",
+	meta_expansion:
+	  [ { label: "Team Members", content: "Ping-Han Soh, An-Tai Li, Jia-Xun Zheng" },
+	  	{ label: "Role", content: "GUI Design and Programming" },
+	  	{ label: "Course", content: "Embedded System Lab" },
+	  	{ label: "Instructor", content: "Professor Sheng-De Wang, Ph.D." } ],
+	links : [ {label: "album", href: "https://picasaweb.google.com/clyde1008li/20090626?feat=directlink" } ],
+	images:
+	  [ "https://lh6.googleusercontent.com/-079Qj_NVWNI/SpFp8uZemjI/AAAAAAAAFU0/_y3V0FdYntw/s400/20090626006.jpg",
+	  	"https://lh4.googleusercontent.com/-JaWXAT0i0qw/SpFqIDR6wPI/AAAAAAAAFVM/cdpdGljT7S8/s400/20090626012.jpg",
+	  	"https://lh6.googleusercontent.com/-3tengWjBg4I/SpFqOm5aBKI/AAAAAAAAFVc/CcquYwzn37k/s400/DSC00391.JPG" ],
+	introduction: 'Created Taipei Metro price information query system written in C (with QT4 library) and executed on PXA300, an embedded system with touch screen.',
+   	tags: ["C", "UI", "project", "NTU"]
+},
+{
+	md5_pid: "",
+	title: "Making Things Easy with Google",
+	header: "Making Things Easy with Google",
+	meta_expansion:
+	  [ { label: "Co-Authors", content: "Chi-Lung Lee, An-Tai Li" },
+	  	{ label: "Chinese Title", content: "用Google輕鬆過生活" },
+	  	{ label: "Publishing", content: "by GrandTech Information Co.(上奇資訊) in 11/2008" },
+	  	{ label: "ISBN", content: "9789866587306" } ],
+	links : [ {label: "博客來", href: "http://www.books.com.tw/exep/prod/booksfile.php?item=0010421376" } ],
+	introduction: 'The book makes in-depth introductions to several web applications and services provided by Google. See how these Google products can help you and how you can benefit from them in daily life. The book is written in Traditional Chinese.',
+   	tags: ["book", "indie"]
+} ]
